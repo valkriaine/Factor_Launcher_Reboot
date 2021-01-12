9a92f019ae5cf2be0c32ba6f9e950e17a6a04c76
Wayne <valkriaine@hotmail.com>
2021-01-12 00:02:24 -0800

    migrated some code to Kotlin

---
+++ app/src/main/java/com/factor/launcher/ui/AnimatedConstraintLayout.kt
@@ -0,0 +1,67 @@
+package com.factor.launcher.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class AnimatedConstraintLayout @SuppressLint("ClickableViewAccessibility") constructor(
+    context: Context,
+    attrs: AttributeSet?
+) :
+    ConstraintLayout(context, attrs) {
+    private var isInDragAndDrop = false
+    private val accelerateDecelerateInterpolator = AccelerateDecelerateInterpolator()
+    fun animateToClickedState() {
+        if (!isInDragAndDrop) {
+            animate().scaleX(0.95f).setInterpolator(accelerateDecelerateInterpolator).duration = 50
+            animate().scaleY(0.95f).setInterpolator(accelerateDecelerateInterpolator).duration = 50
+        }
+    }
+
+    fun animateBackFromClickedState() {
+        if (!isInDragAndDrop) {
+            animate().scaleX(1f).setInterpolator(accelerateDecelerateInterpolator).duration = 50
+            animate().scaleY(1f).setInterpolator(accelerateDecelerateInterpolator).duration = 50
+        }
+    }
+
+    fun animateToSelectedState() {
+        isInDragAndDrop = true
+        animate().scaleX(1.1f)
+            .setInterpolator(accelerateDecelerateInterpolator)
+            .setDuration(200).start()
+        animate().scaleY(1.1f)
+            .setInterpolator(accelerateDecelerateInterpolator)
+            .setDuration(200).start()
+    }
+
+    fun animateToNormalState() {
+        isInDragAndDrop = false
+        animate().scaleX(1f)
+            .setInterpolator(accelerateDecelerateInterpolator)
+            .setDuration(200).start()
+        animate().scaleY(1f)
+            .setInterpolator(accelerateDecelerateInterpolator)
+            .setDuration(200).start()
+    }
+
+    init {
+        setOnTouchListener { _: View?, event: MotionEvent ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    animateToClickedState()
+                    return@setOnTouchListener false
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    animateBackFromClickedState()
+                    return@setOnTouchListener false
+                }
+            }
+            false
+        }
+    }
+}
