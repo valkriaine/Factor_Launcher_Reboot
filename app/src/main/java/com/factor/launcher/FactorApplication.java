67641bc9c8f6604b741e5487cc8a28606b50ce9c
Yifei <valkriaine@outlook.com>
2021-01-16 15:47:58 -0800

    implemented crash report sender (issue #39)

--- app/src/main/java/com/factor/launcher/FactorApplication.java
+++ app/src/main/java/com/factor/launcher/FactorApplication.java
@@ -2,7 +2,18 @@ package com.factor.launcher;
 
 import android.app.Application;
 import com.factor.launcher.managers.AppSettingsManager;
+import org.acra.ACRA;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraMailSender;
+import org.acra.annotation.AcraNotification;
+import org.acra.data.StringFormat;
 
+
+@AcraCore(reportFormat= StringFormat.KEY_VALUE_LIST)
+@AcraNotification(resText = R.string.crash_text,
+        resTitle = R.string.crash_report,
+        resChannelName = R.string.channel_name)
+@AcraMailSender(mailTo = "valkriaine@hotmail.com")
 public class FactorApplication extends Application
 {
     //add other services here
@@ -12,17 +23,17 @@ public class FactorApplication extends Application
     {
         super.onCreate();
 
+        //initialize ACRA
+        ACRA.init(this);
+
         //initialize settings
         AppSettingsManager.getInstance(getApplicationContext());
 
         //todo: remove this for release
-        try {
-            Class.forName("dalvik.system.CloseGuard")
-                    .getMethod("setEnabled", boolean.class)
-                    .invoke(null, true);
-        } catch (ReflectiveOperationException e)
+        try
         {
-            throw new RuntimeException(e);
+            Class.forName("dalvik.system.CloseGuard").getMethod("setEnabled", boolean.class).invoke(null, true);
         }
+        catch (ReflectiveOperationException ignored){}
     }
 }
