df6c7bf72f3315491b48fa86f23a17b29b081e5e
Wayne <valkriaine@hotmail.com>
2022-05-10 18:14:28 -0700

    better home button behavior

--- app/src/main/java/com/factor/launcher/util/OnSystemActionsCallBack.kt
+++ app/src/main/java/com/factor/launcher/util/OnSystemActionsCallBack.kt
@@ -3,7 +3,9 @@ package com.factor.launcher.util
 //implement in fragments to handle onBackPressed
 interface OnSystemActionsCallBack
 {
+    // on back button pressed
     fun onBackPressed(): Boolean
 
+    // on home button pressed
     fun onNewIntent() : Boolean
 }
